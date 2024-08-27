52fd1cf3f56e48de578f66bc3a9af2943164a355
KaspiDoron <kaspidoron@gmail.com>
2024-08-27 18:23:15 +0300

    yes commit

--- landing-page/script.js
+++ landing-page/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*****************************************************************************************/
 /* NAV SECTION */
 /*****************************************************************************************/
@@ -68,6 +70,7 @@ userDropdownIcons.addEventListener("click", function () {
     addOverlayCntrl();
 
     closeLan(); // Close popup if it's open
+    closeMobileMenuFn(); // Close menuMobile if it's open
   }
   // Close dropdown if it's open
   else {
@@ -83,6 +86,7 @@ iconGlobe.addEventListener("click", function () {
     addOverlayCntrl();
 
     closeDropdownUser(); // Close dropdown if it's open
+    closeMobileMenuFn(); // Close menuMobile if it's open
   }
   // Close dropdown if it's open
   else {
@@ -115,4 +119,97 @@ closeMobileMenu.addEventListener("click", function () {
     closeMobileMenuFn();
     removeOverlayCntrl();
   }
+});
+
+/*****************************************************************************************/
+/* HERO SECTION */
+/*****************************************************************************************/
+// title Declaring Elements
+const searchBar = document.querySelector(".search-bar");
+const searchField = document.querySelectorAll(".search-field");
+const inputField = document.querySelectorAll(".input");
+
+let chosenSearchField = 0,
+  chosenInputField = 0;
+
+let isSearchFieldChooseNow = false; // Creating a boolean to handle clicking and UNclicking
+
+// title Helper Functions
+// todo MAKING HELPER CLASSES THAT ADDS AND REMOVE THE "REGULAR MDOE" AND THE "CHOSE MODE"
+
+// title Handling the search bar "chose mode"
+
+// Taking the info about the clicked search field
+function activateElement(event) {
+  const clickedElement = event.target; // Get the clicked element
+
+  // Condition that distinguish between "choose mode" and "regular mode"
+  if (!isSearchFieldChooseNow) {
+    isSearchFieldChooseNow = true; //Once it clicked when it was false, now it's true
+
+    // Condition that distinguish between first time clicking and a second/third.. time while there is a clicked field
+    if (
+      chosenInputField !== clickedElement ||
+      chosenSearchField !== clickedElement
+    ) {
+      // Set the chosenSearchField && chosenInputField to the QS
+      if (
+        clickedElement.classList.contains("where") ||
+        clickedElement.classList.contains("where-input")
+      ) {
+        chosenSearchField = document.querySelector(".where");
+        chosenInputField = document.querySelector(".where-input");
+      } else if (
+        clickedElement.classList.contains("arrive-by") ||
+        clickedElement.classList.contains("arrive-by-input")
+      ) {
+        chosenSearchField = document.querySelector(".arrive-by");
+        chosenInputField = document.querySelector(".arrive-by-input");
+      } else if (
+        clickedElement.classList.contains("check-out") ||
+        clickedElement.classList.contains("check-out-input")
+      ) {
+        chosenSearchField = document.querySelector(".check-out");
+        chosenInputField = document.querySelector(".check-out-input");
+      } else if (
+        clickedElement.classList.contains("travelers") ||
+        clickedElement.classList.contains("travelers-input")
+      ) {
+        chosenSearchField = document.querySelector(".travelers");
+        chosenInputField = document.querySelector(".travelers-input");
+      } else {
+        console.log("there is a bug");
+      }
+
+      // Removing all the css classes from the regular mode
+      searchField.forEach((element) => {
+        element.classList.remove("search-field-regular-mode");
+      });
+      inputField.forEach((element) => {
+        element.classList.remove("input-background-regular-mode");
+      });
+
+      // Adding all the right css classes to the chosen search field and the search bar
+      searchBar.classList.add("search-bar-choose-mode");
+      searchField.forEach((element) => {
+        element.classList.add("search-background-choosing-mode");
+      });
+      inputField.forEach((element) => {
+        element.classList.remove("input-background-in-choose-mode");
+      });
+    }
+    // If it's second time clicking and there is already clicked element
+    else {
+    }
+  }
+
+  // When the user clicked a second time he want
+  else {
+  }
+}
+
+// Add event listeners to all elements with the desired classes
+const elementsWithClasses = document.querySelectorAll(".input, .search-field");
+elementsWithClasses.forEach((element) => {
+  element.addEventListener("click", activateElement);
 });
